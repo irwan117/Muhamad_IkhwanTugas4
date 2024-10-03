590aff05f3c5edaf121868e2bcab056f52aca5f2
irwan117 <irwan.11700@gmail.com>
2024-10-03 14:34:27 +0700

    M_Ikhwan3

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,23 +1,38 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> <?php echo $judul; ?></title>
-  </head>
-  <body>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-e0JMYsd53ii+sc0/bJGFs1CZc+5NDVN2yr8+0RDqr001@h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+  <title><?php echo $judul; ?></title>  
+<nav class="navbar fixed-top navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="">Penjadwalan</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="beranda">Penjadwalan</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="beranda">Beranda</a>
-        <a class="nav-link" href="mahasiswa">Mahasiswa</a>
-        <a class="nav-link" href="">Dosen</a>
-      </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="beranda">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mahasiswa">Mahasiswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="dosen">Dosen</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
